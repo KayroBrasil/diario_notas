f24fe4c3b674eae41ee7e5d5e7af654bd994d9a0
Maximo Monteiro <maximo.henri@gmail.com>
2014-02-19 19:55:20 -0300

    Classe Professor criada
    criação da classe professor com atributos, getters e setters.

---
+++ src/controllers/Professor.java
@@ -0,0 +1,43 @@
+package controllers;
+
+public class Professor {
+	
+	private int cod;
+	private String nome;
+	private String endereco;
+	private int tel;
+	private int disciplina;
+	
+	public int getCod() {
+		return cod;
+	}
+	public void setCod(int cod) {
+		this.cod = cod;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public int getTel() {
+		return tel;
+	}
+	public void setTel(int tel) {
+		this.tel = tel;
+	}
+	public int getDisciplina() {
+		return disciplina;
+	}
+	public void setDisciplina(int disciplina) {
+		this.disciplina = disciplina;
+	}
+	
+	
+}
