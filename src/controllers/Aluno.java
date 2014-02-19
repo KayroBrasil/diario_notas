2cb468f81734b875ce284da9c8f01286d6948eae
Maximo Monteiro <maximo.henri@gmail.com>
2014-02-19 19:41:24 -0300

    Classe Aluno criada
    criação da classe aluno, atributos, getters e setters

---
+++ src/controllers/Aluno.java
@@ -0,0 +1,37 @@
+package controllers;
+
+public class Aluno {
+	private int matricula;
+	private String nome;
+	private String endereco;
+	private int tel;
+	
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public int getTel() {
+		return tel;
+	}
+	public void setTel(int tel) {
+		this.tel = tel;
+	}
+	
+	
+	
+	
+}
